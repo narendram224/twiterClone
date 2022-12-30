c5f4050fb58d56d05fb773c21af0fe338f8343d9
narendram224 <narendram224.nm@gmail.com>
2022-12-30 12:29:50 +0530

    Sidebar Ui done.

--- src/components/SidebarLink/SidebarLink.tsx
+++ src/components/SidebarLink/SidebarLink.tsx
@@ -9,10 +9,12 @@ interface ISidebarLink {
 const SidebarLink: FC<ISidebarLink> = ({ text, Icon, active }) => {
   return (
     <div
-      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation`}
+      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${
+        active && "font-bold"
+      }`}
     >
       <Icon className="h-7 text-white" />
-      <span>{text}</span>
+      <span className="hidden xl:inline">{text}</span>
     </div>
   )
 }
